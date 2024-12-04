e3bcfdb361df282e12912bdd7ce051b8b7a20ead
Yash Joshi <yashjoshi1485@gmail.com>
2024-12-04 16:34:10 +0530

    Delete src directory

--- src/main/java/com/CommUnity/app/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.CommUnity.app.repository;
-import com.CommUnity.app.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByUsername(String username);
-}
-
